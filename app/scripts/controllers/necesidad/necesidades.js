fc1b18fd47f5936d440476ff81efc5b3eabcbff7
Juan Camilo Sarmiento Reyes <jcsarmientor@correo.udistrital.edu.co>
2019-10-31 18:41:39 -0500

    Detalle necesidad: Se muestra la información del detalle, excepto los nombres de las actividades y las metas.
    issue #107

--- app/scripts/controllers/necesidad/necesidades.js
+++ app/scripts/controllers/necesidad/necesidades.js
@@ -172,8 +172,11 @@ angular.module('contractualClienteApp')
                 self.gridApi = gridApiService.filter(self.gridApi, self.cargarDatosNecesidades, $scope);
 
                 self.gridApi.selection.on.rowSelectionChanged($scope, function (row) {
-                    self.necesidad = row.entity;
-                    necesidadService.initNecesidad(row.entity.Id)
+                    necesidadService.getFullNecesidad(row.entity.Id).then(function(response) {
+                        if(response.status === 200) {
+                            self.necesidad = response.data.Body;
+                        }
+                    });
                 });
             }
         };
@@ -188,28 +191,12 @@ angular.module('contractualClienteApp')
             query = typeof (query) === "string" ? [query] : query;
             query.push("EstadoNecesidad.Nombre__not_in:Borrador");
 
-            // var req = administrativaRequest.get('necesidad', $.param({
-            //     limit: self.gridOptions.paginationPageSize,
-            //     offset: offset,
-            //     sortby: "Vigencia,NumeroElaboracion",
-            //     order: "desc",
-            //     query: query.join(",")
-            // }, true));
-
             var req = necesidadesCrudRequest.get('necesidad', $.param({
                 limit: self.gridOptions.paginationPageSize,
                 offset: offset,
                 sortby: "Vigencia",
                 order: "desc"
             }, true));
-
-            // req.data.forEach(function(n){
-            //   if(n.UnidadEjecutora === 1){
-            //             n.UniE= "Rector";
-            //         } else{ 
-            //             n.UniE= "Convenios";
-            //         }
-            // });
 
             req.then(gridApiService.paginationFunc(self.gridOptions, offset));
             return req;
@@ -218,25 +205,30 @@ angular.module('contractualClienteApp')
         self.cargarDatosNecesidades(self.offset, self.query);
 
         $scope.direccionar = function (necesidad) {
-            self.g_necesidad = necesidad;
-            self.numero_el = necesidad.NumeroElaboracion;
-            self.vigencia = necesidad.Vigencia;
-            self.modalidadSel = necesidad.ModalidadSeleccion;
+            necesidadService.getFullNecesidad(necesidad.Id).then(function(response) {
+                if(response.status === 200) {
+                    self.necesidad = response.data.Body;
+                }
+                
+                self.g_necesidad = necesidad;
+                self.numero_el = necesidad.NumeroElaboracion;
+                self.vigencia = necesidad.Vigencia;
+                self.modalidadSel = necesidad.ModalidadSeleccion;
 
-            //para mostrar informacion de rechazo
+                //para mostrar informacion de rechazo
 
 
-            // validaciones para los botones: (estado) && (permisos rol)
-            var aproOrRech = [necesidadService.EstadoNecesidadType.Solicitada.Id, necesidadService.EstadoNecesidadType.Modificada.Id]
-                .includes(necesidad.EstadoNecesidad.Id);
+                // validaciones para los botones: (estado) && (permisos rol)
+                var aproOrRech = [necesidadService.EstadoNecesidadType.Solicitada.Id, necesidadService.EstadoNecesidadType.Modificada.Id]
+                    .includes(necesidad.EstadoNecesidadId.Id);
 
-            self.verBotonAprobarNecesidad = aproOrRech && self.buttons.AprobarNecesidad;
-            self.verBotonRechazarNecesidad = aproOrRech && self.buttons.RechazarNecesidad;
-            self.verBotonEditarNecesidad = (necesidadService.EstadoNecesidadType.Rechazada.Id === necesidad.EstadoNecesidad.Id || necesidadService.EstadoNecesidadType.Solicitada.Id === necesidad.EstadoNecesidad.Id) && necesidadService.EstadoNecesidadType.Solicitada.Id === necesidad.EstadoNecesidad.Id && self.buttons.EditarNecesidad;
-            self.verBotonSolicidadCDPNecesidad = necesidadService.EstadoNecesidadType.Aprobada.Id === necesidad.EstadoNecesidad.Id && self.buttons.SolicitarCDP;
+                self.verBotonAprobarNecesidad = aproOrRech && self.buttons.AprobarNecesidad;
+                self.verBotonRechazarNecesidad = aproOrRech && self.buttons.RechazarNecesidad;
+                self.verBotonEditarNecesidad = (necesidadService.EstadoNecesidadType.Rechazada.Id === necesidad.EstadoNecesidadId.Id || necesidadService.EstadoNecesidadType.Solicitada.Id === necesidad.EstadoNecesidadId.Id) && necesidadService.EstadoNecesidadType.Solicitada.Id === necesidad.EstadoNecesidadId.Id && self.buttons.EditarNecesidad;
+                self.verBotonSolicidadCDPNecesidad = necesidadService.EstadoNecesidadType.Aprobada.Id === necesidad.EstadoNecesidadId.Id && self.buttons.SolicitarCDP;
 
-            $("#myModal").modal();
-
+                $("#myModal").modal();
+            });
         };
 
         self.aprobar_necesidad = function () {
